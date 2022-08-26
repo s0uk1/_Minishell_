966d431f225cdd45d3984deefe515c2179e5da9f
SabbajiSoukaina <sabbajisoukaina@gmail.com>
2022-08-26 18:44:20 +0100

    cd is done! unless i decide to make it perfect perfect but its working now

--- execution/my_mess/my_cd.c
+++ execution/my_mess/my_cd.c
@@ -6,7 +6,7 @@
 /*   By: ssabbaji <ssabbaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 15:42:55 by ssabbaji          #+#    #+#             */
-/*   Updated: 2022/08/25 18:24:24 by ssabbaji         ###   ########.fr       */
+/*   Updated: 2022/08/26 18:35:14 by ssabbaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ char *update_env(t_data *data, char *env, char *upd)
     return (NULL);    
 }
  
-void    buff_chdir(t_data *data, char *cmd, char *cwd)
+void    my_chdir(t_data *data, char *cmd, char *cwd)
 {
     char *new_pwd;
     
@@ -56,6 +56,87 @@ void    buff_chdir(t_data *data, char *cmd, char *cwd)
             
 }
 
+// char	*ft_strchr(const char *str, int c)
+// {
+// 	size_t		i;
+// 	size_t		size;
+
+// 	i = 0;
+// 	size = ft_strlen(str);
+// 	while (i < size)
+// 	{
+// 		if (str[i] == (char )c)
+// 			return ((char *)(str + i));
+// 		i++;
+// 	}
+// 	if (c == '\0')
+// 		return ((char *)(str + i));
+// 	return (NULL);
+// }
+
+// char	*ft_strtrim(char const *s1, char const *set)
+// {
+// 	size_t	start;
+// 	size_t	end;
+// 	char	*str;
+
+// 	str = 0;
+// 	if (s1 != 0 && set != 0)
+// 	{
+// 		start = 0;
+// 		end = ft_strlen(s1);
+// 		while (s1[start] && ft_strchr(set, s1[start]))
+// 			start++;
+// 		while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > start)
+// 			end--;
+// 		str = (char *)malloc(sizeof(char) * (end - start + 1));
+// 		if (str)
+// 			ft_strlcpy(str, &s1[start], end - start + 1);
+// 	}
+// 	return (str);
+// }
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+    dest = (char *)malloc(sizeof(char) * n + 1);
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+void    find_dir(t_data *data, char *pwd, char *upd)
+{
+    int     i;
+    char    *test_pwd;
+
+    i = 0;
+    while(pwd[i])
+        i++;
+    while (pwd[i] != '/')
+        i--;   
+    test_pwd = ft_strncpy(test_pwd, pwd, i);
+    if (chdir(test_pwd))
+        find_dir(data ,test_pwd, upd);
+    else
+    {
+        printf("%s\n",upd);
+        my_chdir(data, test_pwd, upd);
+    }
+}
+
+//this function replicates the behavior of bash when
+// a nested directory is deleted
 void    catch_error(t_data *data)
 {
     char    *old_pwd;
@@ -65,8 +146,9 @@ void    catch_error(t_data *data)
     printf("cd: error retrieving current directory: getcwd:cannot access parent directories: No such file or directory\n");
     update_env(data, "PWD", ft_strjoin(old_pwd, "/.."));
     new_pwd = update_env(data, "OLDPWD", old_pwd);
-    if (chdir(ft_strjoin(old_pwd, "/..")))
-        buff_chdir(data, custom_getenv("HOME", data->lst_env), new_pwd);
+    old_pwd = ft_strjoin(old_pwd, "/..");
+    if (chdir(old_pwd))
+        find_dir(data, old_pwd, old_pwd);
 }
 
 void    my_cd(t_data *data, t_cmd *lst_cmd)
@@ -81,7 +163,7 @@ void    my_cd(t_data *data, t_cmd *lst_cmd)
         if (!cwd)
             catch_error(data);
         else
-            buff_chdir(data , cmd[1], cwd);   
+            my_chdir(data , cmd[1], cwd);   
         // if (chdir(cmd[1]))
         //     perror("chdir() error:");
         // else
--- test.c
+++ test.c
@@ -9,52 +9,95 @@
 # include <termios.h>
 # include <errno.h>
 
-int main ()
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-    int pipes[2];
-    int pid;
-    int fd;
-    char    **cmd1;
-    char    **cmd2;
+	unsigned int	i;
 
-    cmd1 = malloc (sizeof (char *) * 3);
-    cmd2 = malloc (sizeof (char *) * 3);
-    cmd1[0] = "/bin/cat";
-    cmd1[1] = "txt";
-    cmd1[2] = NULL;
-    
-    cmd2[0] = "/usr/bin/grep";
-    cmd2[1] = "main";
-    cmd2[2] = NULL;
-    pipe(pipes);
-    fd = open ("txt", O_CREAT | O_RDWR);
-    if (fd == -1)
-        printf("mok ana\n");
-    pid = fork ();
-    if (pid == -1)
-        return (0);
-    if (pid == 0)
-    {
-        close(pipes[0]);
-        dup2(pipes[1], 1);
-        close(pipes[1]);
-        if(execve(cmd1[0], cmd1, NULL) == -1)
-            return (printf("error\n"));
-    }
-    wait (NULL);
-     pid = fork ();
-    if (pid == -1)
-        return (0);
-    if (pid == 0)
-    {
-        close(pipes[1]);
-        dup2(pipes[0], 0);
-        close(pipes[0]);
-        if(execve(cmd2[0], cmd2, NULL) == -1)
-            return (printf("error\n"));
-    }
-    close(pipes[1]);
-    close(pipes[0]);
-    wait (NULL);
-
+	i = 0;
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
+
+void    find_dir(char *pwd)
+{
+    int     i;
+    char    *test_pwd;
+
+    i = 0;
+    while(pwd[i])
+        i++;
+    while (pwd[i] != '/')
+        i--;
+    test_pwd = ft_strncpy(test_pwd, pwd, i);
+    if (chdir(test_pwd))
+        find_dir(test_pwd);
+    else
+        //change dir to it
+}
+
+
+int main()
+{
+    char pwd[]= "f1/f2/f4/..";
+
+    find_dir(pwd);
+}
+// int main ()
+// {
+//     int pipes[2];
+//     int pid;
+//     int fd;
+//     char    **cmd1;
+//     char    **cmd2;
+
+//     cmd1 = malloc (sizeof (char *) * 3);
+//     cmd2 = malloc (sizeof (char *) * 3);
+//     cmd1[0] = "/bin/cat";
+//     cmd1[1] = "txt";
+//     cmd1[2] = NULL;
+    
+//     cmd2[0] = "/usr/bin/grep";
+//     cmd2[1] = "main";
+//     cmd2[2] = NULL;
+//     pipe(pipes);
+//     fd = open ("txt", O_CREAT | O_RDWR);
+//     if (fd == -1)
+//         printf("mok ana\n");
+//     pid = fork ();
+//     if (pid == -1)
+//         return (0);
+//     if (pid == 0)
+//     {
+//         close(pipes[0]);
+//         dup2(pipes[1], 1);
+//         close(pipes[1]);
+//         if(execve(cmd1[0], cmd1, NULL) == -1)
+//             return (printf("error\n"));
+//     }
+//     wait (NULL);
+//      pid = fork ();
+//     if (pid == -1)
+//         return (0);
+//     if (pid == 0)
+//     {
+//         close(pipes[1]);
+//         dup2(pipes[0], 0);
+//         close(pipes[0]);
+//         if(execve(cmd2[0], cmd2, NULL) == -1)
+//             return (printf("error\n"));
+//     }
+//     close(pipes[1]);
+//     close(pipes[0]);
+//     wait (NULL);
+
+// }
