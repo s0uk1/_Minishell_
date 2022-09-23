d91232f688bda61f4e9457a910b2b7df1b0a13f7
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2022-09-23 14:58:15 +0100

    fixed echo :D

--- execution/my_mess/my_echo.c
+++ execution/my_mess/my_echo.c
@@ -6,7 +6,7 @@
 /*   By: ssabbaji <ssabbaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:42:26 by ssabbaji          #+#    #+#             */
-/*   Updated: 2022/09/23 13:12:36 by ssabbaji         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:51:59 by ssabbaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,11 @@ int	start_index(char **cmd)
 				break ;
 			}
 		}
+		else if (!check_valid(cmd[i]))
+		{
+			idx = i;
+			break ;
+		}
 		idx = i;
 		i++;
 	}
@@ -62,23 +67,19 @@ int	my_echo(t_cmd *cmd_lst)
 {
 	char	**cmd;
 	int		idx;
-	int		i;
 
-	i = 0;
 	cmd = cmd_lst->cmd;
 	if (!cmd[1])
 		printf("\n");
 	if (cmd[1][0] != '-' && cmd[1][1] != 'n')
 	{
-		printf("1 ||||||||||\n");
 		print_rest(cmd, 0, 1);
 		return (0);
 	}
-	else 
-	{
-		printf("2 ||||||||||\n");
-		idx = start_index(cmd);
+	idx = start_index(cmd);
+	if (idx == 1)
+		print_rest(cmd, idx, 1);
+	else
 		print_rest(cmd, idx, 0);
-	}
 	return (0);
 }
