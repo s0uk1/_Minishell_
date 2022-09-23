7b96bd5fa1c03e9a643c9da4c7de4a67eee3cecc
SoukainaSabbaji <sabbajisoukaina@gmail.com>
2022-09-23 13:15:08 +0100

    fixing echo

--- execution/my_mess/my_echo.c
+++ execution/my_mess/my_echo.c
@@ -6,7 +6,7 @@
 /*   By: ssabbaji <ssabbaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:42:26 by ssabbaji          #+#    #+#             */
-/*   Updated: 2022/09/19 16:33:09 by ssabbaji         ###   ########.fr       */
+/*   Updated: 2022/09/23 13:12:36 by ssabbaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,21 +57,28 @@ int	start_index(char **cmd)
 	}
 	return (idx);
 }
-
+//printf("||||||||||\n");
 int	my_echo(t_cmd *cmd_lst)
 {
 	char	**cmd;
 	int		idx;
+	int		i;
 
+	i = 0;
 	cmd = cmd_lst->cmd;
 	if (!cmd[1])
 		printf("\n");
 	if (cmd[1][0] != '-' && cmd[1][1] != 'n')
 	{
+		printf("1 ||||||||||\n");
 		print_rest(cmd, 0, 1);
 		return (0);
 	}
-	idx = start_index(cmd);
-	print_rest(cmd, idx, 0);
+	else 
+	{
+		printf("2 ||||||||||\n");
+		idx = start_index(cmd);
+		print_rest(cmd, idx, 0);
+	}
 	return (0);
 }
